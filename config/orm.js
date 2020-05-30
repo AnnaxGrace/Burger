8f463dfe6bd643831a6134d12d0b79f48b388d0f
AnnaGrace <anna.grace.conover@gmail.com>
2020-05-30 15:58:51 -0500

    added ORM file with three functions one to select, one to insert, one to update

---
+++ config/orm.js
@@ -0,0 +1,33 @@
+
+var connection = require("../config/connection.js")
+
+var orm = {
+
+    selectAll: function(table, cb) {
+        let query = "SELECT * FROM ??";
+        connection.query(query, [table], function(err, result) {
+            if (err) throw err;
+            cb (result);
+         });
+    },
+
+    insertOne: function(table, property, value, cb) {
+        let query = "INSERT INTO ?? (??) VALUES (??)";
+        connection.query(query, [table, property, value], function(err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+
+    },
+
+    updateOne: function(table, tableProperty, tablePropertyValue, id, idVal, cb) {
+        let query = "UPDATE ?? SET ?? = ? WHERE ?? = ? "
+        connection.query(query, [table, tableProperty, tablePropertyValue, id, idVal], function(err, result) {
+            if (err) throw err;
+            cb(result)
+        });
+    }
+
+}
+
+module.exports = orm;
