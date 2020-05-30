e11cdebddcb5c73b176755219d4c77b7b8e60509
AnnaGrace <anna.grace.conover@gmail.com>
2020-05-30 17:06:28 -0500

    Set up skeleton for server.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,4 +2,13 @@ var express = require("express");
 var router = express.Router();
 var newBurger = require("../models/burger.js")
 
+router.get("/", function(req, res) {
+    newBurger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        res.render("index", hbsObject);
+    })
+})
+
 module.exports = router;
